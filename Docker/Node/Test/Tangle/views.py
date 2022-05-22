f4fd7cca336dec7c1a07a4d81ca4c792602554a1
Profscienc <97731268+Profscienc@users.noreply.github.com>
2022-05-22 19:20:09 +0000

    Delete views.py

--- Docker/Node/Test/Tangle/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-
-def home(request):
-    return redirect("http://127.0.0.1:8000/admin")
-# Create your views here.
